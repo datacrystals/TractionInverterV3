5a9021c5bc144a7642466c38eeb6d4e99af18386
Ashley Lee <allilee@ucsc.edu>
2025-04-23 12:57:54 -0700

    Attempted to use canopen functions

--- DynamicBrakeGrid/DynamicBrakeGrid/DynamicBrakeGrid.ino
+++ DynamicBrakeGrid/DynamicBrakeGrid/DynamicBrakeGrid.ino
@@ -10,6 +10,7 @@
 #include "Common/FanController.h"
 #include "Common/Constants.h"
 
+#include "Easy-CANopen/Src/Hardware/Hardware.h"
 #include "Easy-CANopen/Src/Easy_CANopen/Easy_CANopen.h"
 #include <mcp2515.h> // https://github.com/autowp/arduino-MCP2515/archive/master.zip
 
@@ -774,6 +775,16 @@ void loop() {
     statusIndicators.Update();
     faultManager.updateFaultStatus();
     faultManager.clearAutoResetFaults();
+	CANopen master_node = {0};
+	CANopen slave_node = {0}; /* Notice that ONLY one slave node can be connected to the CAN network */
+	/* Set the internal identification */
+	slave_node.slave.this_node_ID = 0x5; /* Or try 0xFF */
+
+	/* Find non configured node ID */
+	Easy_CANopen_Other_Node_Check_If_There_Are_Any_Non_Configured_Nodes_At_The_CAN_Network(&master_node);
+	Easy_CANopen_Thread_Listen_Messages(&slave_node);
+	Easy_CANopen_Thread_Listen_Messages(&master_node);
+	//printf("Non configured node ID found = %s\n", master_node.master.lss.non_configured_node_ID_found == true ? "true" : "false");
 
     // Update fan every 5 milliseconds
     if (millis() - lastFanUpdate >= 5) {
